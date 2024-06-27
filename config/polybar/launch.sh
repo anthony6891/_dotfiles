5331e8f48949eb8a31d9ad9c0e54d8aeb03aaeee
Anthony Solorzano Lopez <asolorzano@codemachine.io>
2024-06-26 20:43:19 -0600

    test

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
 # Terminate already running bar instances
-killall -q polybar
+#killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+#while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar &
+#polybar mybar &
 
-echo "Polybar launched..."
+#echo "Polybar launched..."
+#----------------------------------------------
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/example.log 
+polybar example 2>&1 | tee -a /tmp/example.log & disown
+
+echo "Bars launched..."
